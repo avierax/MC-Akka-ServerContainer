4fbf64fe78b787eee215d6dee0fa96ab3b0073d1
ariel <ariel.viera@gmail.com>
2021-09-26 09:31:06 -0400

    Update the build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-dotnet publish --self-contained -c release -r linux-x64
+dotnet publish -c release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true -r linux-x64 --self-contained
