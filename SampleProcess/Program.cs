7447e7f8ca918273819250b268693e02de532ebd
ariel <ariel.viera@gmail.com>
2021-10-10 18:04:38 -0400

    Add sample process

---
+++ SampleProcess/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SampleProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter command:");
+            var command = Console.ReadLine();
+            Console.WriteLine($"Command is {command}");
+        }
+    }
+}
