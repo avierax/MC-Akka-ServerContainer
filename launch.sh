44a04aefb2e3c533dd8b0b7e5eb379fbf00aa1e7
ariel <ariel.viera@gmail.com>
2021-10-17 16:33:53 -0400

    Fix initialization of environment variables

--- launch.sh
+++ launch.sh
@@ -1,7 +1,2 @@
 #!/bin/env bash 
-echo launching wrapper
-export SERVERDIR="/mnt/c/Minecraft/servers/17-biomemeltpot/server"
-export SERVERJAR="/mnt/c/Minecraft/servers/17-biomemeltpot/server/server.jar"
-export NAMEDBACKUPDIR="/mnt/c/Minecraft/servers/17-biomemeltpot/namedbackups"
-export BACKUPDIR="/mnt/c/Minecraft/servers/17-biomemeltpot/backups"
 ./McServerWrapper/bin/Release/net5.0/linux-x64/McServerWrapper
